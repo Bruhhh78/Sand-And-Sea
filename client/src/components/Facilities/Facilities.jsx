f0418d7c2dcb0b84a6567a93b0da15611854d5b8
Anmol Srivastava <anmolsrivastava678@gmail.com>
2024-10-11 01:24:28 +0530

    Git Initializing Add Property

--- client/src/components/Facilities/Facilities.jsx
+++ client/src/components/Facilities/Facilities.jsx
@@ -86,38 +86,6 @@ export default Facilities;
 // import { useMutation } from "react-query";
 // import { toast } from "react-toastify";
 // import { createResidency } from "../../utils/api";
-// const Facilities = ({
-//   prevStep,
-//   propertyDetails,
-//   setPropertyDetails,
-//   setOpened,
-//   setActiveStep,
-// }) => {
-//   const form = useForm({
-//     initialValues: {
-//       bedrooms: propertyDetails.facilities.bedrooms,
-//       parkings: propertyDetails.facilities.parkings,
-//       bathrooms: propertyDetails.facilities.bathrooms,
-//     },
-//     validate: {
-//       bedrooms: (value) => (value < 1 ? "Must have atleast one room" : null),
-//       bathrooms: (value) =>
-//         value < 1 ? "Must have atleast one bathroom" : null,
-//     },
-//   });
-
-//   const { bedrooms, parkings, bathrooms } = form.values;
-
-//   const handleSubmit = () => {
-//     const { hasErrors } = form.validate();
-//     if (!hasErrors) {
-//       setPropertyDetails((prev) => ({
-//         ...prev,
-//         facilities: { bedrooms, parkings, bathrooms },
-//       }));
-//       mutate();
-//     }
-//   };
 
 //   // ==================== upload logic
 //   const { user } = useAuth0();
@@ -152,45 +120,5 @@ export default Facilities;
 //       setActiveStep(0)
 //       refetchProperties()
 //     }
-
 //   })
 
-//   return (
-//     <Box maw="30%" mx="auto" my="sm">
-//       <form
-//         onSubmit={(e) => {
-//           e.preventDefault();
-//           handleSubmit();
-//         }}
-//       >
-//         <NumberInput
-//           withAsterisk
-//           label="No of Bedrooms"
-//           min={0}
-//           {...form.getInputProps("bedrooms")}
-//         />
-//         <NumberInput
-//           label="No of Parkings"
-//           min={0}
-//           {...form.getInputProps("parkings")}
-//         />
-//         <NumberInput
-//           withAsterisk
-//           label="No of Bathrooms"
-//           min={0}
-//           {...form.getInputProps("bathrooms")}
-//         />
-//         <Group position="center" mt="xl">
-//           <Button variant="default" onClick={prevStep}>
-//             Back
-//           </Button>
-//           <Button type="submit" color="green" disabled={isLoading}>
-//             {isLoading ? "Submitting" : "Add Property"}
-//           </Button>
-//         </Group>
-//       </form>
-//     </Box>
-//   );
-// };
-
-// export default Facilities;
