c747ecbc8465abe73332dc6c44ce8319ed50ccfd
Anmol Srivastava <anmolsrivastava678@gmail.com>
2024-10-12 23:27:17 +0530

    Sand And Sea Deployed

--- client/src/components/Companies/Companies.jsx
+++ client/src/components/Companies/Companies.jsx
@@ -3,15 +3,31 @@ import "./Companies.css";
 
 const Companies = () => {
   return (
-    <section className="c-wrapper">
-      <div className="paddings innerWidth flexCenter c-container">
-        <img src="./prologis.png" alt="compaines-logo" />
-        <img src="./tower.png" alt="compaines-logo" />
-        <img src="./equinix.png" alt="compaines-logo" />
-        <img src="./realty.png" alt="compaines-logo" />
-      </div>
-    </section>
+<section className="c-wrapper">
+<div className=" paddings innerWidth flexCenter c-container">
+  <div className="carousel">
+    <img src="./adani realty.png" alt="company-logo" />
+    <img src="./Godrej Properties.png" alt="company-logo" />
+    <img src="./hiranadani.jpeg.png" alt="company-logo" />
+    <img src="./images.png" alt="company-logo" />
+    <img src="./k raheja.png" alt="company-logo" />
+    <img src="./Kalputraru logo.png" alt="company-logo" />
+    <img src="./L & T realty.jpeg.png" alt="company-logo" />
+    <img src="./prestige logo.jpeg.png" alt="company-logo" />
+    {/* Duplicate images for infinite scrolling effect */}
+    <img src="./adani realty.png" alt="company-logo" />
+    <img src="./Godrej Properties.png" alt="company-logo" />
+    <img src="./hiranadani.jpeg.png" alt="company-logo" />
+    <img src="./images.png" alt="company-logo" />
+    <img src="./k raheja.png" alt="company-logo" />
+    <img src="./Kalputraru logo.png" alt="company-logo" />
+    <img src="./L & T realty.jpeg.png" alt="company-logo" />
+    <img src="./prestige logo.jpeg.png" alt="company-logo" />
+  </div>
+</div>
+</section>
   );
 };
 
 export default Companies;
+
